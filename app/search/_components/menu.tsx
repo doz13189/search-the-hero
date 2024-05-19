611e7ef615e6192bdf57f5a1ba24f0d06042e132
Takahiro Kato <dozo13189@gmail.com>
2024-05-19 18:33:28 +0900

    fix: change to link component

--- app/search/_components/menu.tsx
+++ app/search/_components/menu.tsx
@@ -7,12 +7,13 @@ import { FC } from "react";
 export const Menu: FC = ({
 }) => {
 	const pathname = usePathname();
-	const isHeroPage =  pathname.includes("/search/hero")
+	const isHeroPage = pathname.includes("/search/hero")
 	return (
 		<div className="flex">
-		<div className="flex-1">
-			<div
-				className={`
+			<div className="flex-1">
+				<Link
+					href="/search/hero"
+					className={`
 					m-1
 					p-2
 					flex
@@ -22,26 +23,27 @@ export const Menu: FC = ({
 					rounded-lg
 					${isHeroPage ? "bg-orange" : "bg-soft-orange text-grey"}
 				`}
-			>
-				<Link href="/search/hero">プレイキャラ検索</Link>
+				>
+					プレイキャラ検索
+				</Link>
+			</div>
+			<div className="flex-1">
+				<Link
+					href="/search/memory"
+					className={`
+					m-1
+					p-2
+					flex
+					justify-center
+					border-2
+					border-grey
+					rounded-lg
+					${isHeroPage ? "bg-soft-orange text-grey" : "bg-orange"}
+					`}
+				>
+					メモリー検索
+				</Link>
 			</div>
 		</div>
-		<div className="flex-1">
-			<div
-				className={`
-				m-1
-				p-2
-				flex
-				justify-center
-				border-2
-				border-grey
-				rounded-lg
-				${isHeroPage ? "bg-soft-orange text-grey" : "bg-orange"}
-			`}
-			>
-				<Link href="/search/memory">メモリー検索</Link>
-			</div>
-		</div>
-	</div>
 	);
 };
