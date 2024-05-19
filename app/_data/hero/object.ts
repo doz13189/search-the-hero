18f4ea0f6c9764930a21b455c5fdf1167f98f055
Takahiro Kato <dozo13189@gmail.com>
2024-05-19 19:09:25 +0900

    feat: define chara data type

--- app/_data/hero/object.ts
+++ app/_data/hero/object.ts
@@ -1,19 +1,75 @@
-export const heroes = {
+import { z } from "zod";
+import { HeroesSchema } from "./schema";
+
+export const heroes: z.infer<typeof HeroesSchema> = {
 	heroes: [
 		{
 			id: 1161007,
 			epithet: "穿く灼熱",
 			name: "エンデヴァー",
 			rarity: "UR",
-			// description:
-			// 	'自分のスピード@key1%アップ、恐怖耐性アップ/"力タイプ"、"技タイプ"、"知タイプ"が装備した時、自分のクリティカル率@key2%アップ/"信タイプ"、"破タイプ"が装備した時、自分のディフェンス@key3%アップ',
-			// rarity: "UR",
-			// levels: [
-			// 	{ level: 1, skill: [{ key1: 20, key2: 20, key3: 20 }] },
-			// 	{ level: 2, skill: [{ key1: 25, key2: 20, key3: 20 }] },
-			// 	{ level: 3, skill: [{ key1: 30, key2: 20, key3: 20 }] },
-			// 	{ level: 4, skill: [{ key1: 35, key2: 30, key3: 30 }] },
-			// ],
+			resist: {
+				burning: 100,
+				frozen: 50,
+				frostbite: 50,
+			},
+			critical: 10,
+			defense: 100,
+			plusUltra: {
+				description:
+				'敵単体に@key1%ダメージ、高確率(80%)で燃焼(4ターン)を付与/自分のアクションスキル威力@key2%アップ(4ターン)/敵が燃焼の時、このスキルに貫通を付与',
+				levels: [
+					{ level: 1, skill: [{ key1: 650, key2: 30, key3: null }] },
+					{ level: 2, skill: [{ key1: 650, key2: 30, key3: null }] },
+					{ level: 3, skill: [{ key1: 650, key2: 30, key3: null }] },
+					{ level: 4, skill: [{ key1: 650, key2: 30, key3: null }] },
+					{ level: 5, skill: [{ key1: 650, key2: 30, key3: null }] },
+				],
+			},
+			actionSkill1 : {
+				description:
+				'敵全体に@key1%ダメージ、クリティカル率@key2%ダウン(4ターン)、中確率(55%)で燃焼(4ターン)を付与/自分のHPが50%以上の時、このスキルに貫通を付与/敵にかばう、挑発のキャラがいたら、このスキルのスキル威力@key3%アップ',
+				levels: [
+					{ level: 1, skill: [{ key1: 300, key2: 30, key3: 30 }] },
+					{ level: 2, skill: [{ key1: 300, key2: 30, key3: 30 }] },
+					{ level: 3, skill: [{ key1: 300, key2: 30, key3: 30 }] },
+					{ level: 4, skill: [{ key1: 300, key2: 30, key3: 30 }] },
+					{ level: 5, skill: [{ key1: 300, key2: 30, key3: 30 }] },
+				],
+			},
+			actionSkill2 : {
+				description:
+				'敵全体に@key1%ダメージ/このスキルでCHAIN成功時、自分のプルスウルトラゲージ@key2%アップ/敵に燃焼のキャラがいたら、このスキルに貫通を付与/敵にかばう、挑発のキャラがいたら、このスキルのスキル威力@key3%アップ',
+				levels: [
+					{ level: 1, skill: [{ key1: 300, key2: 30, key3: 30 }] },
+					{ level: 2, skill: [{ key1: 300, key2: 30, key3: 30 }] },
+					{ level: 3, skill: [{ key1: 300, key2: 30, key3: 30 }] },
+					{ level: 4, skill: [{ key1: 300, key2: 30, key3: 30 }] },
+					{ level: 5, skill: [{ key1: 300, key2: 30, key3: 30 }] },
+				],
+			},
+			autoSkill1 : {
+				description:
+				'自分にスタン完全耐性を付与/自分が敵を1人撃破した時、自分のパワー@key1%アップ/自分が戦闘不能になった時、味方全体にくいしばり(2回)を付与、逆境(5ターン)を付与',
+				levels: [
+					{ level: 1, skill: [{ key1: 25, key2: null, key3: null }] },
+					{ level: 2, skill: [{ key1: 25, key2: null, key3: null }] },
+					{ level: 3, skill: [{ key1: 25, key2: null, key3: null }] },
+					{ level: 4, skill: [{ key1: 25, key2: null, key3: null }] },
+					{ level: 5, skill: [{ key1: 25, key2: null, key3: null }] },
+				],
+			},
+			autoSkill2 : {
+				description:
+				'自分のHPが80%以上の時、自分のスキル威力@key1%アップ/自分のHPが50%以上の時、味方全体のスピード@key2%アップ/自分のHPが30%以上の時、自分のパワー@key3%アップ',
+				levels: [
+					{ level: 1, skill: [{ key1: 30, key2: 25, key3: 30 }] },
+					{ level: 2, skill: [{ key1: 30, key2: 25, key3: 30 }] },
+					{ level: 3, skill: [{ key1: 30, key2: 25, key3: 30 }] },
+					{ level: 4, skill: [{ key1: 30, key2: 25, key3: 30 }] },
+					{ level: 5, skill: [{ key1: 30, key2: 25, key3: 30 }] },
+				],
+			},
 			tags: [
 				"allBuffSpeedUP",
 			],
--- app/_data/hero/schema.ts
+++ app/_data/hero/schema.ts
@@ -1,23 +1,41 @@
 import { z } from "zod";
 import { Rarity, Tags } from "../_common/schema";
 
+const LevelsSchema = z.array(
+	z.object({
+		level: z.number(),
+		skill: z.array(
+			z.object({
+				key1: z.number().nullable(),
+				key2: z.number().nullable(),
+				key3: z.number().nullable(),
+			}),
+		),
+	}),
+);
+
+const SkillSchema = z.object({
+	description: z.string(),
+	levels: LevelsSchema,
+});
+
 export const HeroSchema = z.object({
 	id: z.number(),
 	epithet: z.string(),
 	name: z.string(),	
 	rarity: Rarity,
-	// levels: z.array(
-	// 	z.object({
-	// 		level: z.number(),
-	// 		skill: z.array(
-	// 			z.object({
-	// 				key1: z.number(),
-	// 				key2: z.number(),
-	// 				key3: z.number(),
-	// 			}),
-	// 		),
-	// 	}),
-	// ),
+	resist: z.object({
+		burning: z.number(),
+		frozen: z.number(),
+		frostbite: z.number(),
+	}),
+	critical: z.number(),
+	defense: z.number(),
+	plusUltra: SkillSchema,
+	actionSkill1: SkillSchema,
+	actionSkill2: SkillSchema,
+	autoSkill1: SkillSchema,
+	autoSkill2: SkillSchema,
 	tags: z.array(Tags),
 });
 
