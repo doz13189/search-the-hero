43d4ad792a30257b1cef5b99b56de3ace2fdf230
Takahiro Kato <dozo13189@gmail.com>
2024-07-14 17:13:39 +0900

    feat: suspense support

---
+++ app/search/_components/Loading.tsx
@@ -0,0 +1,9 @@
+export const Loading = () => {
+	return (
+		<div className="flex items-center justify-center min-h-screen" >
+			<p>Loading...</p>
+			<img src="/man-superhero-svgrepo-com.svg" alt="Superman Icon" width={30} height={30} />
+		</div>
+	);
+};
+
