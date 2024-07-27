bf70433a7a1d306a33c27ac63d1633e69a9236c1
Takahiro Kato <dozo13189@gmail.com>
2024-07-27 17:08:18 +0900

    chore: fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,58 +3,42 @@ import type { Metadata } from "next";
 import { RocknRoll_One } from "next/font/google";
 import "./globals.css";
 import { Menu } from "./search/_components/menu";
+import Head from 'next/head';
 
 const font = RocknRoll_One({
 	weight: ["400"],
 	subsets: ["cyrillic"],
 });
 
-const serviceTitle = "Search the hero";
-const serviceDescription = "僕のヒーローアカデミア ULTRA IMPACT(ヒロトラ)のプレイキャラ/メモリーを検索することができる非公式サービスです。また、リーク情報は扱いません。";
-const serviceImage = "https://search-the-hero.vercel.app/_next/image?url=%2Fmemory-image%2Fmemory_l_2400152.webp&w=1920&q=75";
-const serviceUrl = "https://search-the-hero.vercel.app";
-export const metadata: Metadata = {
-	title: serviceTitle,
-	description: serviceDescription,
-	openGraph: {
-		type: 'website',
-		url: serviceUrl,
-		title: serviceTitle,
-		description: serviceDescription,
-		images: [
-		  {
-			url: serviceImage,
-			width: 800,
-			height: 600,
-			alt: 'Og Image Alt',
-		  },
-		],
-	  },
-	  twitter: {
-		card: 'summary',
-		title: serviceTitle,
-		description: serviceDescription,
-		images: [
-		  {
-			url: serviceImage,
-			alt: 'Twitter Image Alt',
-		  },
-		],
-	  },
-};
-
 export default function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	// FIXME: おそらくキャッシュの問題でプレイキャラ検索/メモリ検索の切り替え時に色が変わらない
-	// const headersList = headers();
-	// const currentUrl = headersList.get("x-url")
-	// const isCharacterSearchPage = currentUrl?.includes("search/character")
+	const serviceTitle = "Search the hero";
+	const serviceDescription = "僕のヒーローアカデミア ULTRA IMPACT(ヒロトラ)のプレイキャラ/メモリーを検索することができる非公式サービスです。また、リーク情報は扱いません。";
+	const serviceImageUrl = "https://search-the-hero.vercel.app/_next/image?url=%2Fmemory-image%2Fmemory_l_2400152.webp&w=1920&q=75";
+	const serviceUrl = "https://search-the-hero.vercel.app";
 
 	return (
 		<html lang="ja">
+			<Head>
+				<title>{serviceTitle}</title>
+				<meta name="description" content={serviceDescription} />
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content={serviceUrl} />
+				<meta property="og:title" content={serviceTitle} />
+				<meta property="og:description" content={serviceDescription} />
+				<meta property="og:image" content={serviceImageUrl} />
+				<meta property="og:image:width" content={"800"} />
+				<meta property="og:image:height" content={"600"} />
+				<meta property="og:image:alt" content={"Og Image Alt"} />
+				<meta name="twitter:card" content={"summary"} />
+				<meta name="twitter:title" content={serviceTitle} />
+				<meta name="twitter:description" content={serviceDescription} />
+				<meta name="twitter:image" content={serviceImageUrl} />
+				<meta name="twitter:image:alt" content={"Twitter Image Alt"} />
+			</Head>
 			<body className={font.className}>
 				<div className="container mx-auto">
 					{/*
