ec449bd6d3506680eec127cb551c28c362923a1a
Takahiro Kato <dozo13189@gmail.com>
2024-08-01 16:09:44 +0900

    chore: fix

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
 					bg-very-light-gray
 					rounded-lg
 				">
-					<p className="text-xs">{`2024/07/29`}</p>
+					<p className="text-xs">{`2024/08/01`}</p>
 					<p className="text-xs">{`以下のデータ追加に伴うアップデートを実施しました。`}</p>
 					{["1221003"].map((id) => (
 						<div key={id} className="m-2">
